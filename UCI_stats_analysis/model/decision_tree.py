500a789d25f9dc22dfea600b4e49043e033781ae
vgarg-bits <2025aa05819@wilp.bits-pilani.ac.in>
2026-02-07 16:43:25 +0530

    Update decision_tree.py

--- UCI_stats_analysis/model/decision_tree.py
+++ UCI_stats_analysis/model/decision_tree.py
@@ -4,12 +4,15 @@ from sklearn.model_selection import train_test_split
 from utils import evaluate_model
 
 def run_decision_tree(df, target):
-    X = df.drop(columns=[target])
+   # One-hot encode categorical features
+    X = pd.get_dummies(df.drop(columns=[target]), drop_first=True)
     y = df[target]
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     model = DecisionTreeClassifier(max_depth=5, random_state=42)
     model.fit(X_train, y_train)
 
     metrics = evaluate_model(model, X_test, y_test)
+
     return model, metrics
