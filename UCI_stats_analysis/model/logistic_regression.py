3486bea5827471173a6fe72ad96c4e42d57b33c9
vgarg-bits <2025aa05819@wilp.bits-pilani.ac.in>
2026-02-08 09:31:56 +0530

    Update logistic_regression.py

--- UCI_stats_analysis/model/logistic_regression.py
+++ UCI_stats_analysis/model/logistic_regression.py
@@ -1,15 +1,14 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from utils import evaluate_model
+from utils import evaluate_model, preprocess
 
 def run_logistic_regression(df, target):
-    X = df.drop(columns=[target])
-    y = df[target]
+    X, y = preprocess(df, target)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
     metrics = evaluate_model(model, X_test, y_test)
-    return model, metrics
+    return model, metrics, X_test, y_test
